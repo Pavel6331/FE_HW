75c3db9e3e11b5240ae92f3fb4eb55b81a3c931d
Pavlo Konelskiy <98803617+Pavel6331@users.noreply.github.com>
2024-04-11 20:06:24 +0300

    Add files via upload

---
+++ HW-DOM-Pavlo/app.js
@@ -0,0 +1,26 @@
+//1
+// const containerEl = document.getElementById('container');
+// console.log(containerEl);
+
+//2
+// const firstText = document.querySelector('.text');
+// console.log(firstText);
+
+//3
+// const listItems = document.querySelectorAll('.list-item');
+// console.log(listItems);
+
+//4
+// const itemEl = document.getElementById('item');
+// console.log(itemEl);
+
+//5
+// const pEl = document.querySelectorAll('.p');
+// console.log(pEl);
+
+//6
+// console.log(document.body.textContent);
+
+//7
+// const link = document.querySelector('#link');
+// console.log(link);
